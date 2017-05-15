cb301243895f67ad8d7133e567c7cdc900065c01
Valentina Lebedeva <val.dan@tut.by>
2017-05-15 22:39:56 +0300

    no message

--- webapp/src/main/java/com/lebedeva/valentina/hospital/webapp/controllers/DepartmentController.java
+++ webapp/src/main/java/com/lebedeva/valentina/hospital/webapp/controllers/DepartmentController.java
@@ -7,7 +7,6 @@ import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
