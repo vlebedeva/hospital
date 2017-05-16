0091bd11c1e4f4d7ab3e50ce9fd654f503231243
Valentina Lebedeva <val.dan@tut.by>
2017-05-16 10:10:58 +0300

    Add class AllServiceTest

---
+++ services/src/test/java/com/lebedeva/valentina/hospital/services/AllServicesTest.java
@@ -0,0 +1,15 @@
+package com.lebedeva.valentina.hospital.services;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({DepartmentServiceTest.class, DiagnosisServiceTest.class})
+
+public class AllServicesTest {
+
+}
